25fa96ed2bff4581a88ece1699fac16e99f4d0a6
RK <r@ukrdev.com>
2019-05-08 00:49:16 +0300

    Define database instance

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Database instance
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+
+const adapter = new FileAsync(
+  path.join(__dirname, '../db/data.json')
+);
+const db = low(adapter);
+
+db.then(database => {
+  database.defaults({
+  }).write();
+})
+
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
