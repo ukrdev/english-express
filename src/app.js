8604de6b037479bc55cb4f784dca3d9fcdce6cd9
RK <r@ukrdev.com>
2019-05-08 01:09:29 +0300

    Add middleware to serve static files

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ db.then(database => {
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
