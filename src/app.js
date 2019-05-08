876a8fcecf5169b789486209e8ca39b0c86a2ae2
RK <r@ukrdev.com>
2019-05-08 09:36:19 +0300

    Add sessions

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 // Database instance
 const low = require('lowdb');
@@ -26,6 +27,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   res.render('index');
